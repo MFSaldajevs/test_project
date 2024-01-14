7efbd2693a0de998479096a2866d3443cf4fe4a4
MFSaldajevs <maksimsf.saldajevs@gmail.com>
2024-01-14 14:20:32 +0000

    Increase the height of the window.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello please enter the dimensions of the window!");
+        set_params(70,30);
+        double height = Window.getHeight();
+        System.out.println("The height of the window is " + height + " mm.");
     }
 
     public static void set_params(double h, double w){
         Window.set_height(h);
     }
-
 }
--- src/Window.java
+++ src/Window.java
@@ -1,11 +1,15 @@
 public class Window {
     static double height;
-
     {
         height = 0.00;
     }
 
     public static void set_height(double h){
-    height = h;
-}
+        height = h;
+    }
+
+    public static double getHeight(){
+        return height;
+    }
+
 }
