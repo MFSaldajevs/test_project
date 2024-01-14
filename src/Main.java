c9d1c2b719ea9fed48b1f00c0e3b9b1a3f50eca5
MFSaldajevs <maksimsf.saldajevs@gmail.com>
2024-01-14 14:01:58 +0000

    Adding the Window class and a call to it in the main method.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static void set_params(double h, double w){
+        Window.set_height(h);
+    }
+
+}
---
+++ src/Window.java
@@ -0,0 +1,11 @@
+public class Window {
+    static double height;
+
+    {
+        height = 0.00;
+    }
+
+    public static void set_height(double h){
+    height = h;
+}
+}
